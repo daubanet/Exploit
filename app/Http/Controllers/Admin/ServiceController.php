d42b733e673a6f9d9326e998cf86c2fb360ee1f3
Laravel Daily <info@laraveldaily.com>
2021-07-06 17:10:26 +0000

    QuickAdminPanel automatic commit

---
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Service;
+use Gate;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ServiceController extends Controller
+{
+    public function index()
+    {
+        abort_if(Gate::denies('service_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('admin.service.index');
+    }
+
+    public function create()
+    {
+        abort_if(Gate::denies('service_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('admin.service.create');
+    }
+
+    public function edit(Service $service)
+    {
+        abort_if(Gate::denies('service_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('admin.service.edit', compact('service'));
+    }
+
+    public function show(Service $service)
+    {
+        abort_if(Gate::denies('service_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('admin.service.show', compact('service'));
+    }
+}
